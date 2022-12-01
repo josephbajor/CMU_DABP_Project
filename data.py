2adbd547ec07d038df83ce4a9ffcf343673d44e0
josephbajor <josephbajor@gmail.com>
2022-11-30 20:56:55 -0500

    load subset

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 
-def load_NEMSIS(key:str, data_dir: os.PathLike, load_targets: dict):
+def load_NEMSIS_dask(key:str, data_dir: os.PathLike, load_targets: dict):
     
     data_dict = {}
     for f in load_targets.keys():
@@ -22,3 +22,37 @@ def load_NEMSIS(key:str, data_dir: os.PathLike, load_targets: dict):
         data_dict[f] = data
 
     return data_dict
+
+def load_NEMSIS_pandas(data_dir:os.PathLike, load_targets:dict, nrows:int=500000):
+
+    data_dict = {}
+
+    for f in load_targets.keys():
+        data = pd.read_csv(os.path.join(data_dir, f + ".txt"), sep="~\|~", engine="python", nrows=nrows)
+
+        # Remove quotes from headers
+        data.columns = [col.strip("'") for col in data.columns]
+
+        if load_targets[f] is not None:
+            data = data[load_targets[f]]
+
+        data = data.replace(to_replace='\.', value=pd.NA, regex=True)
+
+        data_dict[f] = data
+
+    return data_dict
+
+
+def chain_join(datadict:dict, key:str, start_table:str, type:str='left'):
+    
+    for t in datadict.keys():
+
+        datadict[t] = datadict[t].set_index(key, append=True)
+
+    main = datadict[start_table]
+
+    to_join = [datadict[t] for t in datadict.keys() if t != start_table]
+
+    joined_tables = main.join(to_join, how=type)
+
+    return joined_tables
